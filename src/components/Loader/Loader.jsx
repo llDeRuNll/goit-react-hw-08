0455ab34c28857eabfb224dcc8954042243bc173
llDeRuNll <eduardprudiys@gmail.com>
2025-04-28 04:55:02 -0400

    all

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,21 @@
+import { DotLoader } from "react-spinners";
+import { Box } from "@mui/material";
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <DotLoader size={50} color={"#3f51b5"} />
+    </Box>
+  );
 };
 
 export default Loader;
